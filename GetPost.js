41267a29a9260ab8bc67cb43c5ded667949db19b
nickdmartin <nickdmartin@outlook.com>
2015-11-14 14:13:12 -0800

    added post

--- GetPost.js
+++ GetPost.js
@@ -2,10 +2,14 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3004);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
   res.render('home')
@@ -37,6 +41,18 @@ app.get('/get-loopback-improved',function(req,res){
   var context = {};
   context.dataList = qParams;
   res.render('get-loopback-improved', context);
+});
+
+app.post('/post-loopback-improved', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback-improved', context);
 });
 
 app.get('/other-page',function(req,res){
